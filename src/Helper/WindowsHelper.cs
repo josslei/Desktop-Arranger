5b0d2b5a10e727114b3458ea597867396da9802e
Joss Lei <josslei@163.com>
2021-05-22 13:14:40 +0800

    Framework of window

---
+++ src/Helper/WindowsHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Windows;
+
+namespace Desktop_Arranger.Helper
+{
+    class WindowsHelper
+    {
+        static public void setWindowsBottom(Window window)
+        {
+            // Set to the bottom
+            IntPtr hWnd = new WindowInteropHelper(window).Handle;
+            SetWindowPos(hWnd, new IntPtr(1), 0, 0, 0, 0, 0x0001 | 0x0002 | 0x0010);
+        }
+
+        [DllImport("user32.dll")]
+        static private extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int Width, int Height, uint flags);
+
+    }
+}
---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Desktop_Arranger;
+
+namespace Desktop_Arranger
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Set initial location
+            double ScreenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            this.Top = 40.0;
+            this.Left = ScreenWidth - this.Width - 10.0;
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            Helper.WindowsHelper.setWindowsBottom(this);
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            base.OnStateChanged(e);
+            Helper.WindowsHelper.setWindowsBottom(this);
+        }
+
+        private void TitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
